65b6e8b4bc2ac61848212df6f626433c908e0344
xnscdev <xnscdev@gmail.com>
2021-03-16 12:07:03 -0700

    pitcalc: support arbitrary level progress calculation

--- pitcalc.js
+++ pitcalc.js
@@ -17,14 +17,19 @@ const _tx = document.getElementById("tx");
 const _px = document.getElementById("px");
 const _nx = document.getElementById("nx");
 const _pp = document.getElementById("pp");
+const _sl = document.getElementById("sl");
+const _sb = document.getElementById("sb");
+const _nd = document.getElementById("nd");
+const _np = document.getElementById("np");
 
 // User inputs
-let cp = 0, cl = 1, xnl = 15, fp = false;
+let cp = 0, cl = 1, xnl = 15, fp = false, sl = 120;
 
-_cp.oninput = () => { cp = _cp.value; update(); }
-_cl.oninput = () => { cl = _cl.value; update(); }
-_xnl.oninput = () => { xnl = _xnl.value; update(); }
-_fp.oninput = () => { fp = _fp.checked; update(); }
+_cp.oninput = () => { cp = +_cp.value; update(); }
+_cl.oninput = () => { cl = +_cl.value; update(); }
+_xnl.oninput = () => { xnl = +_xnl.value; update(); }
+_fp.oninput = () => { fp = +_fp.checked; update(); }
+_sl.oninput = () => { sl = +_sl.value; update(); }
 
 function ptx(p) {
     let x = 0;
@@ -53,6 +58,12 @@ function update() {
     }
     else
 	_lb.style.display = "none";
+    if (sl <= cl) {
+	_sb.style.display = "block";
+	ret = true;
+    }
+    else
+	_sb.style.display = "none";
     if (ret)
 	return;
 
@@ -61,6 +72,8 @@ function update() {
 	_px.innerHTML = numfmt(ptx(cp));
 	_nx.innerHTML = 0;
 	_pp.innerHTML = 100;
+	_ll.innerHTML = 120;
+	_nd.innerHTML = 0;
 	return;
     }
 
@@ -79,7 +92,13 @@ function update() {
 	for (let i = 1; i < 50; i++)
 	    rpx -= lx(cp, i);
     }
-    _pp.innerHTML = Math.floor (rpx * 1000000 / (cpx - ppx)) / 10000;
+    _pp.innerHTML = (rpx * 100 / (cpx - ppx)).toFixed(4);
+
+    let nd = xnl;
+    for (let i = cl + 1; i < sl; i++)
+	nd += lx(cp, i);
+    _nd.innerHTML = numfmt(nd);
+    _np.innerHTML = (px * 100 / (px + nd)).toFixed(4);
 }
 
 function numfmt(x) {
